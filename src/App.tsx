be853701803a73bac0172ec794837215a196a979
Stanley <gw.stanley.szeto@gmail.com>
2024-12-05 14:51:32 -0500

    minimal reproduction of the issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,130 @@
-import { ConnectButton } from "thirdweb/react";
-import thirdwebIcon from "./thirdweb.svg";
+import {
+	getContract,
+	parseEventLogs,
+	prepareContractCall,
+	prepareEvent,
+	sendAndConfirmTransaction,
+	sendTransaction,
+	waitForReceipt,
+} from "thirdweb";
+import {
+	ConnectButton,
+	useActiveAccount,
+	useActiveWalletChain,
+} from "thirdweb/react";
+import { concatHex, padHex } from "viem";
 import { client } from "./client";
+import thirdwebIcon from "./thirdweb.svg";
+
+const deployProxyByImplementation = {
+	inputs: [
+		{
+			internalType: "address",
+			name: "implementation",
+			type: "address",
+		},
+		{
+			internalType: "bytes",
+			name: "data",
+			type: "bytes",
+		},
+		{
+			internalType: "bytes32",
+			name: "salt",
+			type: "bytes32",
+		},
+	],
+	name: "deployProxyByImplementation",
+	outputs: [
+		{
+			internalType: "address",
+			name: "deployedProxy",
+			type: "address",
+		},
+	],
+	stateMutability: "nonpayable",
+	type: "function",
+} as const;
+
+const installModule = {
+	inputs: [
+		{
+			internalType: "address",
+			name: "_module",
+			type: "address",
+		},
+		{
+			internalType: "bytes",
+			name: "_data",
+			type: "bytes",
+		},
+	],
+	name: "installModule",
+	outputs: [],
+	stateMutability: "payable",
+	type: "function",
+} as const;
+
+//address: "0xB83db4b940e4796aA1f53DBFC824B9B1865835D5",
+//abi: abi,
+//functionName: "deployProxyByImplementation" as const,
+//args: ["0xa6b59721ac0cad7a4f502914b5872b6782a09085", "0x", salt],
 
 export function App() {
+	const account = useActiveAccount();
+	const chain = useActiveWalletChain();
+
+	const simulate = async () => {
+		if (!chain || !account) return;
+
+		const salt = concatHex(["0x0101", padHex("0x", { size: 30 })])
+			.toString()
+			.slice(0, -2)
+			.concat("10") as `0x${string}`;
+
+		const contract = getContract({
+			client,
+			address: "0xB83db4b940e4796aA1f53DBFC824B9B1865835D5",
+			chain,
+		});
+
+		const deployCoreContractTx = prepareContractCall({
+			contract,
+			method: deployProxyByImplementation,
+			params: ["0xa6b59721ac0cad7a4f502914b5872b6782a09085", "0x", salt],
+		});
+		console.log("deployCoreContractTx: ", deployCoreContractTx);
+		const submittedTx = await sendTransaction({
+			account,
+			transaction: deployCoreContractTx,
+		});
+		console.log("submittedTx: ", submittedTx);
+		const receipt = await waitForReceipt(submittedTx);
+		console.log("receipt: ", receipt);
+
+		const proxyDeployedEvent = prepareEvent({
+			signature:
+				"event ProxyDeployed(address indexed implementation, address proxy, address indexed deployer, bytes data)",
+		});
+		const [event] = parseEventLogs({
+			logs: receipt.logs,
+			events: [proxyDeployedEvent],
+		});
+		const proxyAddress = event?.args?.proxy;
+		console.log("proxyAddress: ", proxyAddress);
+
+		const installModuleTx = prepareContractCall({
+			contract,
+			method: installModule,
+			params: ["0xB96b2328EA4946cf7785B8797a084e27e6aCf062", "0x"],
+		});
+
+		await sendAndConfirmTransaction({
+			account,
+			transaction: installModuleTx,
+		});
+	};
+
 	return (
 		<main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
 			<div className="py-20">
@@ -18,7 +140,12 @@ export function App() {
 					/>
 				</div>
 
-				<ThirdwebResources />
+				<button
+					onClick={() => simulate()}
+					className="border border-zinc-800 p-4 rounded-lg hover:bg-zinc-900 transition-colors hover:border-zinc-700 self-center align-self-center mx-auto"
+				>
+					Simulate core deployment and module installation
+				</button>
 			</div>
 		</main>
 	);
@@ -50,49 +177,5 @@ function Header() {
 				file to get started.
 			</p>
 		</header>
-	);
-}
-
-function ThirdwebResources() {
-	return (
-		<div className="grid gap-4 lg:grid-cols-3 justify-center">
-			<ArticleCard
-				title="thirdweb SDK Docs"
-				href="https://portal.thirdweb.com/typescript/v5"
-				description="thirdweb TypeScript SDK documentation"
-			/>
-
-			<ArticleCard
-				title="Components and Hooks"
-				href="https://portal.thirdweb.com/typescript/v5/react"
-				description="Learn about the thirdweb React components and hooks in thirdweb SDK"
-			/>
-
-			<ArticleCard
-				title="thirdweb Dashboard"
-				href="https://thirdweb.com/dashboard"
-				description="Deploy, configure, and manage your smart contracts from the dashboard."
-			/>
-		</div>
-	);
-}
-
-function ArticleCard(props: {
-	title: string;
-	href: string;
-	description: string;
-}) {
-	return (
-		<a
-			href={`${props.href}?utm_source=vite-template`}
-			target="_blank"
-			className="flex flex-col border border-zinc-800 p-4 rounded-lg hover:bg-zinc-900 transition-colors hover:border-zinc-700"
-			rel="noreferrer"
-		>
-			<article>
-				<h2 className="text-lg font-semibold mb-2">{props.title}</h2>
-				<p className="text-sm text-zinc-400">{props.description}</p>
-			</article>
-		</a>
 	);
 }
